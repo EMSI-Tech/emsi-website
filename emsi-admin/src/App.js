b2d758efabbf272386a36447a39e0b4c74677206
huawillian <huawillian@gmail.com>
2019-01-17 00:24:45 -0500

    WH: Jenkins env to project test

--- emsi-admin/src/App.js
+++ emsi-admin/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> EMSI-Admin test</h1>
+        <input id="fileInput" type="file"></input>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- emsi-server/index.js
+++ emsi-server/index.js
@@ -1,11 +1,40 @@
 // Set Up Dependencies
 const express = require('express');
 const morgan = require('morgan');
+const fetch = require('node-fetch');
+const admin = require('firebase-admin');
 
 // Configure Express App
 const app = express();
 const port = 3000;
 app.use(morgan('tiny'));
+
+
+// Configure Firebase App
+admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+      clientEmail: `${process.env.CLIENT_EMAIL}`,
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: `${process.env.DATABASE_URL}`
+  });
+
+  admin.auth().createUser({
+    email: `user${Math.floor(Math.random() * 10000)}@example.com`,
+    emailVerified: false,
+    password: "secretPassword",
+    displayName: "John Doe",
+    photoURL: "http://www.example.com/12345678/photo.png",
+    disabled: false
+  })
+    .then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log("Successfully created new user:", userRecord.uid);
+    })
+    .catch(function(error) {
+      console.log("Error creating new user:", error);
+    });
 
 // Serve build files for emsi-client and emsi-server
 app.use('/admin', express.static('emsi-admin/build'));
@@ -14,4 +43,8 @@ app.use('/', express.static('emsi-client/build'));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+console.log('map api key', process.env.MAPS_API_KEY);
+fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=40.7365834%20-74.1872004&destination=Parsippany&key=${process.env.MAPS_API_KEY}`)
+    .then(res => res.json()).then(res => console.log(res));
